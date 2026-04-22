cce5ac90b51ea2de5d4f59094d980bd2b709a07e
mydimons <mydimons.kb@gmail.com>
2026-04-22 14:21:53 -0700

    popup window

--- Flappy-Bird/Flappy-Bird/Main.cpp
+++ Flappy-Bird/Flappy-Bird/Main.cpp
@@ -1,1 +1,20 @@
 #include "Main.h"
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+
+	int code = MessageBoxA(0, "context", "title", MB_OKCANCEL);
+
+	if (code == IDOK) {
+		cout << "ok";
+	}
+	else if (code == IDCANCEL) {
+		cout << "cancel";
+	}
+
+	cin.get();
+	return 0;
+}
