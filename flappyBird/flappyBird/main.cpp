94b87cc4fa084dd76655f3d9b420f7f68e6b2823
mydimons <mydimons.kb@gmail.com>
2026-05-16 23:36:11 -0700

    program launching without errors!

--- flappyBird/flappyBird/main.cpp
+++ flappyBird/flappyBird/main.cpp
@@ -1,8 +1,15 @@
 #include <SDL3/SDL.h>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-	SDL_Init(SDL_INIT_EVERYTHING);
+int main(int argc, char** argv) {
+
+	SDL_Init(SDL_INIT_VIDEO);
+
+	while (true) {
+		cout << "WORKING" << endl;
+	}
+
 	return 0;
 }
