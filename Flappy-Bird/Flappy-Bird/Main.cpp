cb8f37429264ad956ffff344edab903bf8ecec8b
mydimons <mydimons.kb@gmail.com>
2026-06-03 23:41:16 -0700

    Start of Key class
    currently borken

--- Flappy-Bird/Flappy-Bird/Main.cpp
+++ Flappy-Bird/Flappy-Bird/Main.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
 #include <windows.h>
+#include <array>
 
 using namespace std;
 
+class Key {
+public:
+  Key(int aKeys[3]) {
+    for (int i = 0; i < sizeof(aKeys); i++) {
+      keys[i] = aKeys[i];
+    }
+
+    keys[0] = aKeys[0];
+  }
+private:
+  int keys[];
+};
+
 class KeyboardInput {
 public:
+  //int const jumpKeys[3] = { 32, 38, 87 };
+  int test[3] = { 32, 38, 87 };
+  //Key jumpKeys(test);
+  //int const quitKey = 
+  // 27, 81
+  /// <summary>
+  /// Sets krec to the keyboards pressed key
+  /// </summary>
+  /// <param name="krec"></param>
+  /// <returns></returns>
   bool getconchar(KEY_EVENT_RECORD& krec)
   {
     DWORD cc;
@@ -28,17 +52,26 @@ public:
     }
     return false;
   }
+
+  /// <summary>
+  /// Outputs the pressed key to the console
+  /// </summary>
+  /// <param name="krec"></param>
+  void outputPressedKey(KEY_EVENT_RECORD krec) {
+    cout << "key: " << krec.uChar.AsciiChar
+      << " | code: " << krec.wVirtualKeyCode << endl;
+  }
 };
 
 
 int main()
 {
+  int arr[] = { 3 };
   KeyboardInput keyboardInput;
   KEY_EVENT_RECORD key;
   for (; ; )
   {
     keyboardInput.getconchar(key);
-    std::cout << "key: " << key.uChar.AsciiChar
-      << " code:  " << key.wVirtualKeyCode << std::endl;
+    keyboardInput.outputPressedKey(key);
   }
 }
