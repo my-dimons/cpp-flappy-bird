d922183fc8353a650ad6808d49db6ea6c87a5d1d
mydimons <mydimons.kb@gmail.com>
2026-06-03 22:26:48 -0700

    Getting keyboard input

--- Flappy-Bird/Flappy-Bird/Main.cpp
+++ Flappy-Bird/Flappy-Bird/Main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
-int main() {
-	cout << "Hello World!";
+class KeyboardInput {
+public:
+  bool getconchar(KEY_EVENT_RECORD& krec)
+  {
+    DWORD cc;
+    INPUT_RECORD irec;
+    HANDLE h = GetStdHandle(STD_INPUT_HANDLE);
 
-	return 0;
+    if (h == NULL)
+    {
+      return false; // console not found
+    }
+
+    for (; ; )
+    {
+      ReadConsoleInput(h, &irec, 1, &cc);
+      if (irec.EventType == KEY_EVENT
+        && ((KEY_EVENT_RECORD&)irec.Event).bKeyDown)
+      {
+        krec = (KEY_EVENT_RECORD&)irec.Event;
+        return true;
+      }
+    }
+    return false;
+  }
+};
+
+
+int main()
+{
+  KeyboardInput keyboardInput;
+  KEY_EVENT_RECORD key;
+  for (; ; )
+  {
+    keyboardInput.getconchar(key);
+    std::cout << "key: " << key.uChar.AsciiChar
+      << " code:  " << key.wVirtualKeyCode << std::endl;
+  }
 }
